42c305d152c7a11ac2a8487e4e24b8f351e32a3b
9-sho-5 <hosho.kusu@icloud.com>
2025-06-18 21:17:48 +0900

    add: count

---
+++ Count/Count/CountApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountApp.swift
+//  Count
+//
+//  Created by ほしょ on 2025/06/18.
+//
+
+import SwiftUI
+
+@main
+struct CountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
