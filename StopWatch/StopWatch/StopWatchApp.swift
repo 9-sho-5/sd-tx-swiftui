ded1041e0f2e16e5fa94fb769fefba761394df9a
9-sho-5 <hosho.kusu@icloud.com>
2025-06-18 21:55:26 +0900

    add: StopWatch

---
+++ StopWatch/StopWatch/StopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchApp.swift
+//  StopWatch
+//
+//  Created by ほしょ on 2025/06/18.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
