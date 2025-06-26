5d6babcb29839f66502162cfa848db1dcca7dfed
9-sho-5 <hosho.kusu@icloud.com>
2025-06-26 18:15:21 +0900

    fix: bookがないときに表示するテキストの表示位置を修正

--- booklist/booklist/LibraryView.swift
+++ booklist/booklist/LibraryView.swift
@@ -6,28 +6,31 @@ struct LibraryView: View {
     
     var body: some View {
         NavigationStack {
-            ScrollView {
+            ZStack {
                 if favoriteBooks.isEmpty {
                     Text("登録された本がありません")
                         .foregroundColor(.gray)
                         .padding()
-                }
-                
-                LazyVGrid(columns: columns, spacing: 0) {
-                    ForEach(favoriteBooks, id: \.id) { book in
-                        AsyncImage(url: URL(string: book.thumbnail ?? "")) { image in
-                            image
-                                .resizable()
-                                .scaledToFit()
-                                .shadow(color: Color.black.opacity(0.15), radius: 8, x: 0, y: 4)
-                        } placeholder: {
-                            ProgressView()
+                } else {
+                    ScrollView {
+                        LazyVGrid(columns: columns, spacing: 0) {
+                            ForEach(favoriteBooks, id: \.id) { book in
+                                AsyncImage(url: URL(string: book.thumbnail ?? "")) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                        .shadow(color: Color.black.opacity(0.15), radius: 8, x: 0, y: 4)
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .padding()
+                            }
                         }
                         .padding()
                     }
                 }
-                .padding()
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
             .onAppear {
                 favoriteBooks = SwiftDataManager().fetchAll()
             }
