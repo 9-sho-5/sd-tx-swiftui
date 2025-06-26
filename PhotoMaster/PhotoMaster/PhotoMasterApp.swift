6b44a3cf47e2595fc8b46710bface079cda21acc
9-sho-5 <hosho.kusu@icloud.com>
2025-06-26 16:47:27 +0900

    add: photomaster

---
+++ PhotoMaster/PhotoMaster/PhotoMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMasterApp.swift
+//  PhotoMaster
+//
+//  Created by ほしょ on 2025/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
