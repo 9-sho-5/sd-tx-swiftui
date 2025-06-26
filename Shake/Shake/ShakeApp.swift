277b247bfe49c978daacf556418e7a90e48524c0
9-sho-5 <hosho.kusu@icloud.com>
2025-06-26 17:11:04 +0900

    add: shake

---
+++ Shake/Shake/ShakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakeApp.swift
+//  Shake
+//
+//  Created by ほしょ on 2025/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ShakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StartView()
+        }
+    }
+}
